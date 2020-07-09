526e3c6f6c2ca635a8002b356d8cdc18c91b0a6c
theusaf <theusafyt@gmail.com>
2020-07-09 15:07:14 -0700

    Update examples

--- examples/basicUsage.js
+++ examples/basicUsage.js
@@ -1,7 +1,9 @@
 var Kahoot = require("kahoot.js-updated");
 var client = new Kahoot;
 console.log("Joining kahoot...");
-client.join(9802345 /* Or any other kahoot token */, "kahoot.js");
+client.join(9802345 /* Or any other kahoot token */, "kahoot.js").catch(e=>{
+	console.log("Failed to join the game. Error: " + e);
+});
 client.on("joined", () => {
 	console.log("I joined the Kahoot!");
 });
