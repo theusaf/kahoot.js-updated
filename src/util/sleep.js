fd98f233ad92e06e8975572e589ca79ba8826b14
theusaf <theusafyt@gmail.com>
2020-10-02 11:43:40 -0700

    Update docs [skip ci]

--- src/util/sleep.js
+++ src/util/sleep.js
@@ -1,5 +1,8 @@
 /**
  * @fileinfo This module exports a function used for delays
+ * sleep - sleeps
+ * @param {Number} n The number of seconds to wait
+ * @returns {Promise} Resolves after n seconds
  */
 module.exports = function sleep(n){
   return new Promise((resolve)=>{
